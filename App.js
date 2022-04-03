c52ecf00fab0955854afbe39e1980621bd64ef34
Jesse <101078250+JessePython@users.noreply.github.com>
2022-04-03 12:53:22 -0700

    First Save

---
+++ App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          Edit <code>src/App.js</code> and save to reload. :D
+          Good stuff!
+        </p>
+        <h1>Hello World! :)</h1>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ NewApp.js
@@ -0,0 +1,24 @@
+function NewApp() {
+  return (
+    <div className="NewApp">
+      <header className="App-header">
+        
+        <p>
+          Edit <code>src/App.js</code> and save to reload. :D
+          NEWWWWWWWW
+        </p>
+        <h1>Hello World! :) eat an egg</h1>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default NewApp;
